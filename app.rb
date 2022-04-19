81ec274a7dce897b4fdb5be65422fb4bdbe35895
Kyle <khartson2017@gmail.com>
2022-04-19 17:37:29 -0400

    completed assignment

---
+++ app.rb
@@ -0,0 +1,29 @@
+# app.rb
+# puts 'Hello World!'
+
+# puts 
+# puts "Hello World!"
+# puts "Hello World!"
+# puts "Hello World!"
+
+# prints 
+# print "Hello World!"
+# print "Hello World!"
+# print "Hello World!"
+
+# inspecting data
+# puts [1,2,3]
+# => puts [1,2,3].inspect => converts to string
+# p [1,2,3]
+
+# pp 
+# pp [
+#     {:id=>1, :hello=>"World"},
+#     {:id=>2, :hello=>"Ruby"},
+#     {:id=>3, :hello=>"Moon"},
+#     {:id=>4, :hello=>"Learner"}
+# ]
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
